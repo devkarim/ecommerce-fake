4a6a3b92b756232e75504b1d10803273916a6069
Karim <devkarim@hotmail.com>
2023-08-06 02:55:11 +0300

    Fix not using https in axios client

--- src/services/client.ts
+++ src/services/client.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const client = axios.create({
   baseURL:
     process.env.NODE_ENV == 'production'
-      ? 'http://admin.ecommerce-fake.karimwael.com/api'
+      ? 'https://admin.ecommerce-fake.karimwael.com/api'
       : 'http://localhost:3001/api',
   timeout: 5000,
   responseType: 'json',
