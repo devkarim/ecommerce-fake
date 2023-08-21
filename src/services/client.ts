2a5a04dbd49a82933730d82a9f8f8752625ee005
Karim <devkarim@hotmail.com>
2023-08-21 23:21:58 +0300

    refactor: add isProd and isDev to constants
    Add global constant variables for checking development and production.

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
+import { isProd } from '@/config/constants';
+
 const client = axios.create({
-  baseURL:
-    process.env.NODE_ENV == 'production'
-      ? 'https://admin.ecommerce-fake.karimwael.com/api'
-      : 'http://localhost:3001/api',
+  baseURL: isProd
+    ? 'https://admin.ecommerce-fake.karimwael.com/api'
+    : 'http://localhost:3001/api',
   timeout: 5000,
   responseType: 'json',
 });
